265e71a7e974f904ecea78d74df830ea071c07bd
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-09-23 10:12:54 +0200

    Log

---
+++ Pages/Sailors/Log/LogIn.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SejlKlubsApp.Models;
+using SejlKlubsApp.Services.Interfaces;
+using SejlKlubsApp.Services.Service;
+
+namespace SejlKlubsApp.Pages.Sailors.Log
+{
+    public class LogInModel : PageModel
+    {
+        private ISailorService sailorService;
+        private LogInService loginService;
+        public string AccessDenied = "";
+        [BindProperty]
+        public Sailor Sailor { get; set; }
+        public LogInModel(ISailorService service, LogInService logIn)
+        {
+            sailorService = service;
+            loginService = logIn;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            foreach (Sailor sailor in sailorService.GetAllSailorsAsync().Result)
+            {
+                if (sailor.Name == Sailor.Name)
+                {
+                    if (sailor.Password == Sailor.PasswordCheck)
+                    {
+                        loginService.SailorLogIn(sailor);
+                        return RedirectToPage("/Index");
+                    }
+                }
+                AccessDenied = "Navnet/kodeordet eksistere ikke";
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Sailors/Log/LogOut.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SejlKlubsApp.Services.Service;
+
+namespace SejlKlubsApp.Pages.Sailors.Log
+{
+    public class LogOutModel : PageModel
+    {
+        private LogInService logInService;
+        public LogOutModel(LogInService logIn)
+        {
+            logInService = logIn;
+        }
+        public IActionResult OnGet()
+        {
+            logInService.SailorLogOut();
+            return RedirectToPage("");
+        }
+    }
+}
